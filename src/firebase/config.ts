4bafaea584275efacaba6671c60b94f1c907fce6
8GYM SPORT <8gymsport@gmail.com>
2025-10-31 09:02:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4578372126-6de93",
+  "appId": "1:1040387001113:web:82a5b162d2e5f5e7d91388",
+  "apiKey": "AIzaSyAHiUtzE3WJoUYzRaaboSy4HLhJ61167Yc",
+  "authDomain": "studio-4578372126-6de93.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1040387001113"
+};
