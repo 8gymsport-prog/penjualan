93e25c7be986b3dad501e4cb78e6f84d4d1110ee
8GYM SPORT <8gymsport@gmail.com>
2025-11-08 09:29:11 +0000

    hapus bacaan Dashboard Penjualan

--- src/components/pages/dashboard-page.tsx
+++ src/components/pages/dashboard-page.tsx
@@ -149,9 +149,6 @@ export default function DashboardPage() {
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <h2 className="text-2xl font-bold font-headline tracking-tight">
-          Dashboard Penjualan
-        </h2>
         <div className="flex flex-col gap-4 md:gap-8">
           <SalesOverview transactions={transactions} />
           <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
